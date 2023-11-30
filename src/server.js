ec77a105e9b79706bbaa791bbbf672b16219d61f
Kevin <kevinsilvarj10@gmail.com>
2023-11-30 07:00:07 -0300

    Adicionar URL do back-end no CROS config

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173/"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173/", "https://foodexplorer-backend-8v1b.onrender.com"],
   credentials: true
 }));
 
