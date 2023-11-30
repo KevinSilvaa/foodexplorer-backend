166ff0298de682591778cca6dc1990fae0eeca48
Kevin <kevinsilvarj10@gmail.com>
2023-11-30 08:33:15 -0300

    Validar usuário via token

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -32,6 +32,7 @@ class SessionsController {
     });
 
     response.cookie("token", token, {
+      httpOnly: true,
       sameSite: "none",
       secure: true,
       maxAge: 30 * 60 * 1000
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,15 @@
 const { Router } = require("express");
 
 const UsersController = require("../controllers/UsersController");
+const UsersValidatedController = require("../controllers/UsersValidatedController");
 const usersRoutes = Router();
 
 const usersController = new UsersController();
+const usersValidatedController = new UsersValidatedController();
+
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.get("/validated", ensureAuthenticated, usersValidatedController.index);
 
 module.exports = usersRoutes;
